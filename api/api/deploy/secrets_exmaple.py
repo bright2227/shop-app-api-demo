043390ac0484edb0f6e892d72f00fb1056ef7dd5
bright2227 <bright2227@gmail.com>
2020-09-04 00:41:33 +0800

    put a secrets_example.py

---
+++ api/api/deploy/secrets_exmaple.py
@@ -0,0 +1,10 @@
+# change the key and files me to secret when deploying
+SECRET_KEY = "rj3vhyKiDRNmth75sxJemvOuTy1Hy0ogeKgS9JP8Gp7dGDctfSMUuOt5QbSpsS9xAlvBMTXW3Z6VTODvvFcV3TmtrZUbGkHBcs8I"
+
+DATABASE_USER = "dbuser"
+DATABASE_PASSWORD = "password"
+
+CACHES_PASSWORD = "yourpassword"
+
+EMAIL_HOST_USER = "youremail@gmail.com"
+EMAIL_HOST_PASSWORD = "youremailpass"
