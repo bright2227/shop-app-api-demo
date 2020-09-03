b201ab5ab8c1d27f5b45b293e3f19c4d62036cf1
bright2227 <bright2227@gmail.com>
2020-09-03 23:55:22 +0800

    merge settings with shop app projects

---
+++ api/api/deploy/dev.py
@@ -0,0 +1,41 @@
+from api.deploy import secrets
+import os
+
+
+SECRET_KEY = secrets.SECRET_KEY
+
+ALLOWED_HOSTS = ['*']
+
+
+#Cache
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/0", 
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
+    }
+}
+
+
+# celery
+broker_url = 'redis://127.0.0.1:6379/0'
+result_backend = 'redis://127.0.0.1:6379/0'
+accept_content = ["json"]
+task_serializer = "json"
+result_serializer = "json"
+# task_annotations = {'tasks.my_task': {'rate_limit': '10/s'}}
+task_time_limit = 10 * 60
+task_soft_time_limit = 10 * 60
+# beat_scheduler = "django_celery_beat.schedulers:DatabaseScheduler"
+
+
+#email
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_HOST_USER = secrets.EMAIL_HOST_USER
+EMAIL_HOST_PASSWORD = secrets.EMAIL_HOST_PASSWORD
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
---
+++ api/api/deploy/prod.py
@@ -0,0 +1,57 @@
+from api.deploy import secrets
+import os
+
+
+SECRET_KEY = secrets.SECRET_KEY
+
+ALLOWED_HOSTS = ['*']
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'api', 
+        'USER': secrets.DATABASE_USER, 
+        'PASSWORD': secrets.DATABASE_PASSWORD, 
+        'HOST': 'db', 
+        'PORT':'3306', 
+    }
+}
+
+
+#Cache
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6379/1", 
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "PASSWORD": secrets.CACHES_PASSWORD, 
+        },
+    }
+}
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "default"
+CACHE_TTL = 60 * 1
+
+
+# celery
+broker_url = "redis://redis:6379/2"
+result_backend = "redis://redis:6379/3"
+accept_content = ["json"]
+task_serializer = "json"
+result_serializer = "json"
+# task_annotations = {'tasks.my_task': {'rate_limit': '10/s'}}
+task_time_limit = 10 * 60
+task_soft_time_limit = 10 * 60
+# beat_scheduler = "django_celery_beat.schedulers:DatabaseScheduler"
+
+
+#email
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_HOST_USER = secrets.EMAIL_HOST_USER
+EMAIL_HOST_PASSWORD = secrets.EMAIL_HOST_PASSWORD
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
