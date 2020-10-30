368bcfcafed7e4c578023384851d32b7b9471f7c
bright2227 <bright2227@gmail.com>
2020-10-30 23:19:07 +0800

    change base url in axios

--- frontend/src/axios-api.js
+++ frontend/src/axios-api.js
@@ -1,12 +1,12 @@
 import axios from 'axios'
 
 const getAPI = axios.create({
-    baseURL: 'http://localhost',
+    // baseURL: 'http://localhost',
     timeout: 5000,
 })
 
 const getAPIwithToken = axios.create({
-    baseURL: 'http://localhost',
+    // baseURL: 'http://localhost',
     timeout: 5000,
 })
 
